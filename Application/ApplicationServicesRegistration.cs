45ef540602cf631aff8a5be4d128c713620f5d4a
Muhammad Danish <danishkanirya9999@gmail.com>
2025-09-10 07:29:46 +0500

    Adding AutoMapper functionality, adding DTOs, and registring service of automapper inside application layer

---
+++ Application/ApplicationServicesRegistration.cs
@@ -0,0 +1,19 @@
+﻿using Application.Profiles;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    public static class ApplicationServicesRegistration
+    {
+        public static void ConfigureApplicationServices(this IServiceCollection services)
+        {
+          services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ Application/Profiles/MappingProfile.cs
@@ -0,0 +1,23 @@
+﻿using Application.DTOs;
+using Application.DTOs.LeaveRequest;
+using AutoMapper;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Profiles
+{
+    public class MappingProfile:Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<LeaveRequest,LeaveRequestDto>().ReverseMap();
+            CreateMap<LeaveRequest,LeaveRequestListDto>().ReverseMap();
+            CreateMap<LeaveType,LeaveTypeDto>().ReverseMap();
+            CreateMap<LeaveAllocation,LeaveAllocationDto>().ReverseMap();
+        }
+    }
+}
