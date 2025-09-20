380f391f50c916ed55ad5296ffb8cfa3b3be9c58
Muhammad Danish <danishkanirya9999@gmail.com>
2025-09-20 14:44:21 +0500

    handling some exception for debugging purpose

--- src/API/API/Controllers/LeaveTypesController.cs
+++ src/API/API/Controllers/LeaveTypesController.cs
@@ -1,4 +1,5 @@
 ﻿using Application.DTOs.LeaveType;
+using Application.Exceptions;
 using Application.Features.LeaveTypes.Requests.Commands;
 using Application.Features.LeaveTypes.Requests.Queries;
 using MediatR;
@@ -44,6 +45,42 @@ namespace API.Controllers
             var command = new CreateLeaveTypeCommand { LeaveTypeDto=leaveTypeDto };
             var response = await _mediator.Send(command);
             return Ok(response);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Put( [FromBody] LeaveTypeDto leaveTypeDto)
+        {
+            try
+            {
+                var command = new UpdateLeaveTypeCommand { LeaveTypeDto = leaveTypeDto };
+                await _mediator.Send(command);
+                return NoContent();
+            }
+            catch (NotFoundException ex)
+            {
+                return NotFound(ex.Message); // 404
+            }
+            catch (BadRequestException ex)
+            {
+                return BadRequest(ex.Message); // 400
+            }
+            }
+
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                var command = new DeleteLeaveTypeCommand { Id=id };
+                await _mediator.Send(command);
+                return NoContent();
+            }
+            catch (NotFoundException ex)
+            {
+                return NotFound(ex.Message); // 404
+            }
+
         }
     }
 }
