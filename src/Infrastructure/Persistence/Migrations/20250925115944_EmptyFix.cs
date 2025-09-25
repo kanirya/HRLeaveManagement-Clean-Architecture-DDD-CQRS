26120b95cd11172f095ca7e72e2a69b2b194f808
Muhammad Danish <danishkanirya9999@gmail.com>
2025-09-25 17:00:33 +0500

    adding migration

---
+++ src/Infrastructure/Persistence/Migrations/20250925115944_EmptyFix.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class EmptyFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+          
+
+         
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
