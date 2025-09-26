f9b30143b1fa770e083e20249b0f3f78f66557ee
Muhammad Danish <danishkanirya9999@gmail.com>
2025-09-26 16:56:20 +0500

    starting up authentication with jwt token

---
+++ src/Core/Domain/Auth/RefreshToken.cs
@@ -0,0 +1,17 @@
+﻿namespace Domain.Auth
+{
+    public class RefreshToken
+    {
+        public int Id { get; set; }
+        public string Token { get; set; } = string.Empty;
+        public DateTime Expires { get; set; }
+        public bool IsRevoked { get; set; }
+        public DateTime Created { get; set; }
+        public string CreatedByIp { get; set; } = string.Empty;
+        public DateTime? Revoked { get; set; }
+        public string RevokedByIp { get; set; } = string.Empty;
+        public string ReplacedByToken { get; set; } = string.Empty;
+        public Guid UserId { get; set; }
+        public bool IsActive => !IsRevoked && DateTime.UtcNow <= Expires;
+    }
+}
---
+++ src/Core/Domain/Auth/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Auth
+{
+    public class User
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string Role { get; private set; }
+        private User() { }
+        public User(Guid id, string name, string email, string role = "User")
+        {
+            Id = id == Guid.Empty ? Guid.NewGuid() : id;
+            Name = name;
+            Email = email;
+            Role = string.IsNullOrWhiteSpace(role) ? "User" : role;
+        }
+    }
+}
