1b40bf6b1e830afa78efbac2e8087ca6ac579cea
Muhammad Danish <danishkanirya9999@gmail.com>
2025-09-27 16:46:10 +0500

    refreshtoken repo and handle retry task

---
+++ src/Infrastructure/Persistence/Repositories/Auth/RefreshTokenRepository.cs
@@ -0,0 +1,35 @@
+﻿using Application.Contracts.Persistence.Auth;
+using Domain.Auth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Repositories.Auth
+{
+    public class RefreshTokenRepository : IRefreshTokenRepository
+    {
+       private readonly ApplicationDbContext _context;
+        public RefreshTokenRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(RefreshToken token)
+        {
+           await _context.RefreshTokens.AddAsync(token);
+        }
+
+        public async Task<RefreshToken?> GetByTokenAsync(string token)
+        {
+            return await _context.RefreshTokens.FindAsync(token);
+        }
+
+
+        public  async Task SaveChangesAsync()
+        {
+           await  _context.SaveChangesAsync();
+        }
+    }
+}
