4541d0e42c9d7a31b94b78350b78f01f4c13069e
Muhammad Danish <danishkanirya9999@gmail.com>
2025-11-03 20:15:13 +0500

    Setting up Unit Testing project

---
+++ Test/Controllers/LeaveTypeControllerTest.cs
@@ -0,0 +1,22 @@
+﻿using Application.Persistence.Contracts;
+using AutoMapper;
+using FakeItEasy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Controllers
+{
+    public  class LeaveTypeControllerTest
+    {
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+        private readonly IMapper _mapper;
+        public LeaveTypeControllerTest()
+        {
+            _leaveTypeRepository= A.Fake<ILeaveTypeRepository>();
+            _mapper=A.Fake<IMapper>();
+        }
+    }
+}
