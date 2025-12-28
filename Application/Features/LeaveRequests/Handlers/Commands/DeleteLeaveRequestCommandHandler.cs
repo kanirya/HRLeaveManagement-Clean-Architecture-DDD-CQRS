f93a3fea1353898937468c2ef4cebd42aaf86e41
Muhammad Danish <danishkanirya9999@gmail.com>
2025-12-28 22:30:52 +0500

    completing CQRS (with MediatR)

---
+++ Application/Features/LeaveRequests/Handlers/Commands/DeleteLeaveRequestCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using Application.Features.LeaveRequests.Requests.Commands;
+using Application.Persistence.Contracts;
+using AutoMapper;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.LeaveRequests.Handlers.Commands
+{
+    public class DeleteLeaveRequestCommandHandler:IRequestHandler<DeleteLeaveRequestCommand>
+    {
+        private readonly ILeaveRequestRepository _leaveRequestRepository;
+        private readonly IMapper _mapper;
+
+        public DeleteLeaveRequestCommandHandler(ILeaveRequestRepository leaveRequestRepository, IMapper mapper)
+        {
+            _leaveRequestRepository=leaveRequestRepository;
+            _mapper=mapper;
+        }
+
+        public async Task<Unit> Handle(DeleteLeaveRequestCommand request, CancellationToken cancellationToken)
+        {
+           var leaveRequest=await _leaveRequestRepository.Get(request.Id);
+            if (leaveRequest==null)
+            {
+                throw new Exception($"Leave request with id {request.Id} not found.");
+            }
+            await _leaveRequestRepository.Delete(leaveRequest);
+            return Unit.Value;
+        }
+    }
+}
