581140aac2728b2a667cd245947c70a44fff941b
Muhammad Danish <danishkanirya9999@gmail.com>
2025-12-28 22:30:51 +0500

    Adding AutoMapper functionality, adding DTOs, and registring service of automapper inside application layer

---
+++ Application/DTOs/LeaveAllocationDto.cs
@@ -0,0 +1,19 @@
+﻿using Application.DTOs.Common;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs
+{
+    public class LeaveAllocationDto:BaseDto
+    {
+        public int NumberOfDays { get; set; }
+        public LeaveTypeDto LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+        public int Period { get; set; }
+        public string EmployeeId { get; set; }
+    }
+}
