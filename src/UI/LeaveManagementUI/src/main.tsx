528b2d5c0864186a13598aa02a629141e115c24d
Muhammad Danish <danishkanirya9999@gmail.com>
2025-12-28 22:31:14 +0500

    setting up tailwind and routes

--- src/UI/LeaveManagementUI/src/main.tsx
+++ src/UI/LeaveManagementUI/src/main.tsx
@@ -2,9 +2,38 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import Layout from "./Layout.tsx"
+import Login from "./features/auth/pages/Login.tsx";
+import Register from "./features/auth/pages/Register.tsx";
+import Dashboard from "./features/admin/pages/Dashboard.tsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout />,
+        // errorElement: <GlobalErrorPage />,
+        children: [
+            { index: true, element: <App /> },
+            { path: "/login", element: <Login /> },
+            { path: "/register", element: <Register /> },
+            // {
+            //     element: <PrivateRoute roles={["Admin"]} />,
+            //     children: [{ path: "/dashboard/:id?", element: <Dashboard /> }],
+            // },
+            // {
+            //     element: <PrivateRoute  />,
+            //     children: [{path:"/:id", element: <Profile /> }],
+            // }
+        ],
+    },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+
+          <RouterProvider router={router} />
+
+
   </StrictMode>,
 )
