4b3850a395f8620cbe65944550d469f4352cc406
Muhammad Danish <danishkanirya9999@gmail.com>
2025-12-28 22:31:22 +0500

    testing our first handler

--- Test/Controllers/LeaveTypeControllerTest.cs
+++ Test/Controllers/LeaveTypeControllerTest.cs
@@ -1,22 +1,57 @@
-﻿using Application.Persistence.Contracts;
+﻿using Application.DTOs.LeaveType;
+using Application.Features.LeaveTypes.Handlers.Queries;
+using Application.Features.LeaveTypes.Requests.Queries;
+using Application.Persistence.Contracts;
 using AutoMapper;
+using Domain;
 using FakeItEasy;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Moq;
 
-namespace Test.Controllers
+public class GetLeaveTypeListRequestHandlerTests
 {
-    public  class LeaveTypeControllerTest
+    private readonly Mock<ILeaveTypeRepository> _mockRepo;
+    private readonly Mock<IMapper> _mockMapper;
+    private readonly GetLeaveTypeListRequestHandler _handler;
+
+    public GetLeaveTypeListRequestHandlerTests()
     {
-        private readonly ILeaveTypeRepository _leaveTypeRepository;
-        private readonly IMapper _mapper;
-        public LeaveTypeControllerTest()
+        _mockRepo = new Mock<ILeaveTypeRepository>();
+        _mockMapper = new Mock<IMapper>();
+
+        _handler = new GetLeaveTypeListRequestHandler(_mockRepo.Object, _mockMapper.Object);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldReturnMappedLeaveTypeList_WhenDataExists()
+    {
+        // Arrange
+        var leaveTypes = new List<LeaveType>
         {
-            _leaveTypeRepository= A.Fake<ILeaveTypeRepository>();
-            _mapper=A.Fake<IMapper>();
-        }
+            new LeaveType { Id = 1, Name = "Annual", DefaultDays = 10 },
+            new LeaveType { Id = 2, Name = "Sick", DefaultDays = 5 }
+        };
+
+        var leaveTypeDtos = new List<LeaveTypeDto>
+        {
+            new LeaveTypeDto { Id = 1, Name = "Annual", DefaultDays = 10 },
+            new LeaveTypeDto { Id = 2, Name = "Sick", DefaultDays = 5 }
+        };
+
+        _mockRepo.Setup(r => r.GetLeaveTypesListWithDetails(It.IsAny<CancellationToken>()))
+                 .ReturnsAsync(leaveTypes);
+
+        _mockMapper.Setup(m => m.Map<List<LeaveTypeDto>>(leaveTypes))
+                   .Returns(leaveTypeDtos);
+
+        // Act
+        var result = await _handler.Handle(new GetLeaveTypeListRequest(), CancellationToken.None);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(2, result.Count);
+        Assert.Equal("Annual", result[0].Name);
+
+        _mockRepo.Verify(r => r.GetLeaveTypesListWithDetails(It.IsAny<CancellationToken>()), Moq.Times.Once);
+        _mockMapper.Verify(m => m.Map<List<LeaveTypeDto>>(leaveTypes), Moq.Times.Once);
     }
 }
