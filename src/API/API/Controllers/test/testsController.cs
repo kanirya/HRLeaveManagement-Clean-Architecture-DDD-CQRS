05e1e1bd6232ad6fbf2eb491a3a1b70635311333
Muhammad Danish <danishkanirya9999@gmail.com>
2025-12-28 22:31:19 +0500

    customzing global exception

---
+++ src/API/API/Controllers/test/testsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers.test
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class testsController : ControllerBase
+    {
+        [HttpGet("test-error")]
+        public IActionResult TestError()
+        {
+            throw new Exception("Something went wrong!");
+        }
+
+
+        [HttpGet("Text-Auth")]
+        [Authorize]
+        public IActionResult UnAuthorize()
+        {
+            return Ok("Jwt is working");
+        }
+    }
+}
