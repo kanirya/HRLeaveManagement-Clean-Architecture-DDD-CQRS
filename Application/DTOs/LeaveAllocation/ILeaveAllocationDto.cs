cc70a445dde2b800d59a577b8a227c962aa4088d
Muhammad Danish <danishkanirya9999@gmail.com>
2025-12-28 22:30:53 +0500

    Adding Generic validation

--- Application/DTOs/LeaveAllocation/ILeaveAllocationDto.cs
+++ Application/DTOs/LeaveAllocation/ILeaveAllocationDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Application.DTOs.LeaveType;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,9 @@ using System.Threading.Tasks;
 
 namespace Application.DTOs.LeaveAllocation
 {
-    public class CreateLeaveAllocationDto:ILeaveAllocationDto
+    public interface ILeaveAllocationDto
     {
         public int NumberOfDays { get; set; }
         public int LeaveTypeId { get; set; }
-        public int Period { get; set; }
     }
 }
---
+++ Application/DTOs/LeaveRequest/Validators/UpdateLeaveRequestDtoValidator.cs
@@ -0,0 +1,22 @@
+﻿using Application.Persistence.Contracts;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.LeaveRequest.Validators
+{
+    public class UpdateLeaveRequestDtoValidator : AbstractValidator<UpdateLeaveRequestDto>
+    {
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+        public UpdateLeaveRequestDtoValidator(ILeaveTypeRepository leaveTypeRepository)
+        {
+            _leaveTypeRepository = leaveTypeRepository;
+
+            Include(new ILeaveRequestDtoValidator(_leaveTypeRepository));
+
+           }
+    }
+}
