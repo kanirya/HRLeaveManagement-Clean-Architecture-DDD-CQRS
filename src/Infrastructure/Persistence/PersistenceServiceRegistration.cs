be73651651d3ddd8c6fa586c3cab1f339ed973d0
Muhammad Danish <danishkanirya9999@gmail.com>
2025-12-28 22:30:58 +0500

    completing the persistence project by adding class of registring services and make a connection of db

---
+++ src/Infrastructure/Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,30 @@
+﻿
+using Application.Persistence.Contracts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceCollection ConfigurePersistenceServices(this IServiceCollection services,IConfiguration configuration)
+        {
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                 options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+           
+            services.AddScoped<ILeaveTypeRepository, LeaveTypeRepository>();
+            services.AddScoped<ILeaveAllocationRepository, LeaveAllocationRepository>();
+            services.AddScoped<ILeaveRequestRepository, LeaveRequestRepository>();
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            return services;
+        }
+    }
+}
